38b276303de6130d6475818a9e153035c2ec694a
abdullaalfahad <aafahad02@gmail.com>
2025-11-18 15:22:37 +0600

    chore: initialize nextjs with shadcn, tanstack query, xior

---
+++ lib/xior.ts
@@ -0,0 +1,21 @@
+import { getCookie } from "cookies-next";
+import xior from "xior";
+
+const api = xior.create({
+  baseURL: "localhost:4000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = getCookie("accessToken") as string | undefined;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
